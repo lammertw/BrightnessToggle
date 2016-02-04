876b2137325948888df2de530ebff13d47654fc5
Lammert Westerhoff <westerhoff@gmail.com>
2016-02-04 09:29:45 +0100

    Initial Commit

---
+++ BrightnessToggle/BrightnessToggle.h
@@ -0,0 +1,19 @@
+//
+//  BrightnessToggle.h
+//  BrightnessToggle
+//
+//  Created by Lammert Westerhoff on 04/02/16.
+//  Copyright © 2016 Xebia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrightnessToggle.
+FOUNDATION_EXPORT double BrightnessToggleVersionNumber;
+
+//! Project version string for BrightnessToggle.
+FOUNDATION_EXPORT const unsigned char BrightnessToggleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrightnessToggle/PublicHeader.h>
+
+
